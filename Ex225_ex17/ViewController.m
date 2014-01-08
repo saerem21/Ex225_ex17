08eb4c1c91ce70751497c6aaed692edc50d88110
saerem21 <nemessisdds>
2014-01-08 12:22:25 +0900

    애니메이션 변형 테스트입니다.
    231page

--- Ex225_ex17/ViewController.m
+++ Ex225_ex17/ViewController.m
@@ -9,58 +9,57 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
-@implementation ViewController {
-    BOOL moveFlag;
-}
+@implementation ViewController
 
 - (IBAction)series:(id)sender {
-    [UIView beginAnimations:@"series1" context:NULL];
-    [UIView setAnimationDelegate:self];
-    [UIView setAnimationDuration:1.0];
-    //delegate
-    [UIView setAnimationDelegate:self];
-    [UIView setAnimationDidStopSelector:@selector(changeBackgroundClear)];
+    //연속 애니메이션
+    [self rotate:nil];
+    //delay 따로
+    [self performSelector:@selector(combination:) withObject:nil afterDelay:0.7];
+}
+
+//원상복귀
+- (IBAction)setOriginal:(id)sender {
+    [UIView beginAnimations:@"original" context:nil];
+    [UIView setAnimationDuration:0.7];
     
-    self.imageView.backgroundColor = [UIColor blackColor];
+    self.imageView.transform = CGAffineTransformIdentity;
+    
+    [UIView commitAnimations];
+    
+   }
+
+//두개의 트랜스폼 결합
+- (IBAction)combination:(id)sender {
+    CGAffineTransform t1 = CGAffineTransformMakeScale(1.2, 1.2);
+    CGAffineTransform t2 = CGAffineTransformMakeTranslation(30, 30);
+    CGAffineTransform comb = CGAffineTransformConcat(t1, t2);
+    CGAffineTransform t = CGAffineTransformConcat(self.imageView.transform, comb);
+    
+    [UIView beginAnimations:@"id_move" context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDuration:0.7];
+    
+    self.imageView.transform = t;
+    
+    [UIView commitAnimations];
+   
+}
+
+- (IBAction)rotate:(id)sender {
+    [UIView beginAnimations:@"id_rotate" context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDuration:0.7];
+    
+    self.imageView.transform = CGAffineTransformRotate(self.imageView.transform, M_PI);
     
     [UIView commitAnimations];
    }
 
-- (void)changeBackgroundClear {
-    [UIView beginAnimations:@"series2" context:NULL];
-    [UIView setAnimationDuration:1.0];
-    [UIView setAnimationDelegate:self];
-    
-    self.imageView.backgroundColor = [UIColor clearColor];
-    
-    [UIView commitAnimations];
-}
-
-- (IBAction)move:(id)sender {
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationDuration:0.7];
-    
-    if (moveFlag) {
-        self.imageView.center = CGPointMake(self.imageView.center.x + 100, self.imageView.center.y + 100);
-    }else {
-        self.imageView.center = CGPointMake(self.imageView.center.x - 100, self.imageView.center.y - 100);
-    }
-    moveFlag = !moveFlag;
-    
-    [UIView commitAnimations];
-    
-    }
-
-- (IBAction)alpha:(id)sender {
-    
-    [UIView animateWithDuration:0.7 animations:^{
-        self.imageView.alpha = (self.imageView.alpha == 0.0) ? 1.0 : 0.0;
-    }];
-}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
