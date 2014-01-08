d3434e1e55a4ce4573c387b00d71ff16c979ba02
saerem21 <nemessisdds>
2014-01-08 12:12:38 +0900

    애니메이션 테스트입니다..
    225page

--- Ex225_ex17/ViewController.m
+++ Ex225_ex17/ViewController.m
@@ -10,14 +10,61 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    BOOL moveFlag;
+}
 
+- (IBAction)series:(id)sender {
+    [UIView beginAnimations:@"series1" context:NULL];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDuration:1.0];
+    //delegate
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(changeBackgroundClear)];
+    
+    self.imageView.backgroundColor = [UIColor blackColor];
+    
+    [UIView commitAnimations];
+   }
+
+- (void)changeBackgroundClear {
+    [UIView beginAnimations:@"series2" context:NULL];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationDelegate:self];
+    
+    self.imageView.backgroundColor = [UIColor clearColor];
+    
+    [UIView commitAnimations];
+}
+
+- (IBAction)move:(id)sender {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.7];
+    
+    if (moveFlag) {
+        self.imageView.center = CGPointMake(self.imageView.center.x + 100, self.imageView.center.y + 100);
+    }else {
+        self.imageView.center = CGPointMake(self.imageView.center.x - 100, self.imageView.center.y - 100);
+    }
+    moveFlag = !moveFlag;
+    
+    [UIView commitAnimations];
+    
+    }
+
+- (IBAction)alpha:(id)sender {
+    
+    [UIView animateWithDuration:0.7 animations:^{
+        self.imageView.alpha = (self.imageView.alpha == 0.0) ? 1.0 : 0.0;
+    }];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
